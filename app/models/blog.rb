7301ab92e5796fcf358db733c1ed6cc2bb309a5b
arknamal <arknamal@gmail.com>
2025-05-23 05:47:10 +0500

    add validations for blog attributes - to mimic real scenario

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,11 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :api_responses
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :author, presence: true
+  # We can add more validations (like a min limit for length etc. if needed - these are basic) e.g.
+  # validates :title, length: { minimum: 5 }
+  # validates :body, length: { minimum: 10 }
 end
